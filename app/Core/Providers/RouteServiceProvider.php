9cc8dd7f7932e4dfaf2983828db0a570afb6f35f
Jefferson Frade <jefferrson.frade@gmail.com>
2020-07-21 17:21:32 -0300

    Removed API and Controllers

--- app/Core/Providers/RouteServiceProvider.php
+++ app/Core/Providers/RouteServiceProvider.php
@@ -42,21 +42,6 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function map()
     {
-        $this->mapApiRoutes();
-    }
 
-    /**
-     * Define the "api" routes for the application.
-     *
-     * These routes are typically stateless.
-     *
-     * @return void
-     */
-    protected function mapApiRoutes()
-    {
-        Route::prefix('api')
-             ->middleware('api')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/api.php'));
     }
 }
